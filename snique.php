30988572dc58415237136d7b105e7868a1dc1477
Philip Graham Willoughby <willerz+github@gmail.com>
2012-06-04 20:22:58 +0100

    Try messing with the thumb URL

--- snique.php
+++ snique.php
@@ -14,39 +14,13 @@ class snique
 	{
 		if ( strpos($_SERVER['REQUEST_URI'], 'wp-admin') === false )
 		{
-			add_filter('wp_get_attachment_thumb_file', array(&$this, 'get_attachment_thumb_file'),20,2);
+			add_filter('wp_get_attachment_thumb_url', array(&$this, 'get_attachment_thumb_url'),20,2);
 		}
 	}
 	
-	function get_attachment_thumb_file($file,$id)
+	function get_attachment_thumb_url($url,$id)
 	{
-		$etag = get_post_meta($id,'snique_etag',true);
-		if (strlen($etag) < 1)
-		{
-			srand(intval($num));
-			$chars = 'abcdef0123456789';
-			while (strlen($etag) < 32)
-			{
-				$etag .= $chars[rand(0, 16)];
-			}
-		}
-		$if_none_match = isset($_SERVER['HTTP_IF_NONE_MATCH']) ?
-			 stripslashes($_SERVER['HTTP_IF_NONE_MATCH']) : 
-				 false ;
-		if( false !== $if_none_match )
-		{
-			$tags = split( ", ", $if_none_match ) ;
-			foreach( $tags as $tag )
-			{
-				if( $tag == $etag )
-				{
-					header( "HTTP/1.1 304 NOT MODIFIED" );
-					exit;
-				}
-			}
-		}
-		header( "Cache-Control: public, max-age=60, no-transform, must-revalidate" );
-		header( 'Etag: "'.$etag.'"' );
+		return site_url('/%07images/'.$id);
 	}
 }
 
